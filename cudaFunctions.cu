5dbd96d164f1754b521a54d084af209784e25628
linuxu <linuxu@ParallelC23-12.academic.management.afeka.local>
2023-06-26 19:33:44 +0300

    update

--- cudaFunctions.cu
+++ cudaFunctions.cu
@@ -14,117 +14,80 @@ __device__ double calcDistance(const Point* p1, const Point* p2, double* t) {
     return distance;
 }
 
-__global__ void checkProximityCriteria(int* count, const Point* points, double* tValues, const int tCount, const int N, const int K, const double D, Point* resultPoints, double* resultT) {
+__global__ void checkProximityCriteria(Point* resultPoints,double* resultT, int* count, Point *points, double *tValues, const int tCount,const int N,const int K, const double D){
     int idx = blockIdx.x * blockDim.x + threadIdx.x; // point idx
 
-    if (idx < tCount) {
+    if (idx < tCount)
+    {
+        
         double* t = &(tValues[idx]);
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N && j != i; j++) {
+            for(int j = 0; j < N && j != i; j++) {
                 double distance = calcDistance(&points[i], &points[j], t);
-
-                if (distance <= D) {
-                    int resultIdx = atomicAdd(count, 1);
-                    if (resultIdx < MAX_RESULTS) {
-                        resultPoints[resultIdx] = points[i];
-                        resultT[resultIdx] = *t;
-                    }
-                    if (*count >= K) {
-                        break;
-                    }
+                int resultIdx = atomicAdd(count, 1);
+                if (resultIdx < N * tCount) {
+                    resultPoints[resultIdx] = points[i];
+                    resultT[resultIdx] = *t;
                 }
-            }
+                if (*count >= K) {
+                    break;
+                }
+            }         
         }
     }
 }
 
-void computeOnGPU(int* N, int* K, double* D, int* tCountSize, double* myTValues, Point* points, Point* resultPoints, double* resultT) {
+
+void computeOnGPU(int *N, int *K, double *D, int *tCountSize, double *myTValues, Point *points) {
     // Error code to check return values for CUDA calls
     cudaError_t err = cudaSuccess;
 
-    int blocksPerGrid = ((*tCountSize) + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    int blocksPerGrid = ((*tCountSize) + (*N)) / BLOCK_SIZE < 1 ? 1 : ((*tCountSize) + (*N)) / BLOCK_SIZE < 1; 
     int threadsPerBlock = BLOCK_SIZE;
 
-    Point* dPoints;     // point for device
-    double* dTValues;   // tValues for device
+    Point *dPoints;     // point for device
+    Point* resultPoints;
+    double *dTValues;   // tValues for device  
+    double* resultT;  
 
     err = cudaMalloc(&dPoints, (*N) * sizeof(Point));
-    if (err != cudaSuccess) {
+    if (err != cudaSuccess){
         fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
     err = cudaMalloc(&dTValues, (*tCountSize) * sizeof(double));
-    if (err != cudaSuccess) {
+    if (err != cudaSuccess){
+        fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+    err = cudaMalloc(&resultPoints, (*N)*(*tCountSize) * sizeof(Point));
+    if (err != cudaSuccess){
+        fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+    err = cudaMalloc(&resultT, (*N)*(*tCountSize) * sizeof(double));
+    if (err != cudaSuccess){
         fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
     err = cudaMemcpy(dPoints, points, (*N) * sizeof(Point), cudaMemcpyHostToDevice);
-    if (err != cudaSuccess) {
+    if (err != cudaSuccess){
         fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
     err = cudaMemcpy(dTValues, myTValues, (*tCountSize) * sizeof(double), cudaMemcpyHostToDevice);
-    if (err != cudaSuccess) {
+    if (err != cudaSuccess){
         fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
 
     int count = 0;
 
-    Point* dResultPoints;  // Result points for device
-    double* dResultT;      // Result t values for device
-
-    err = cudaMalloc(&dResultPoints, MAX_RESULTS * sizeof(Point));
-    if (err != cudaSuccess) {
-        fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-    err = cudaMalloc(&dResultT, MAX_RESULTS * sizeof(double));
-    if (err != cudaSuccess) {
-        fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    checkProximityCriteria<<<blocksPerGrid, threadsPerBlock>>>(
-        &count, dPoints, dTValues, *tCountSize, *N, *K, *D, dResultPoints, dResultT
-    );
-
-    printf("Count: %d\n", count);
-
-    // Copy the results back to host memory
-    Point* resultPointsHost = (Point*)malloc(MAX_RESULTS * sizeof(Point));
-    double* resultTHost = (double*)malloc(MAX_RESULTS * sizeof(double));
-
-    err = cudaMemcpy(
-        resultPointsHost, dResultPoints, MAX_RESULTS * sizeof(Point), cudaMemcpyDeviceToHost
-    );
-    if (err != cudaSuccess) {
-        fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-    err = cudaMemcpy(
-        resultTHost, dResultT, MAX_RESULTS * sizeof(double), cudaMemcpyDeviceToHost
-    );
-    if (err != cudaSuccess) {
-        fprintf(stderr, "Error in line %d (error code %s)!\n", __LINE__, cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-
-    // Copy the valid results to the output arrays
-    int validResults = (count < MAX_RESULTS) ? count : MAX_RESULTS;
-    for (int i = 0; i < validResults; i++) {
-        resultPoints[i] = resultPointsHost[i];
-        resultT[i] = resultTHost[i];
-    }
-
-    // Free device allocations
+    checkProximityCriteria<<<blocksPerGrid, threadsPerBlock>>>(resultPoints, resultT, &count, dPoints, dTValues, *tCountSize, *N, *K, *D);
+    printf("Count: %d\n",count);
+    
+    // free device allocation
     cudaFree(dPoints);
     cudaFree(dTValues);
-    cudaFree(dResultPoints);
-    cudaFree(dResultT);
-
-    // Free host memory
-    free(resultPointsHost);
-    free(resultTHost);
 }
