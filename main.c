7cc46c8988be0a0668f9993033d766d53ea24e42
linuxu <linuxu@ParallelC23-2.academic.management.afeka.local>
2023-06-27 13:36:16 +0300

    ad

--- main.c
+++ main.c
@@ -88,6 +88,8 @@ void gatherResults(int rank, int size, int N, int tCount, int tCountSize, int **
       }
    }
 
+   printf("%d %d",rank,recvcounts[rank]);
+   printf("%d %d",rank,displs[rank]);
    // // Gather the 2D array results from all processes into global_results on rank 0
    // MPI_Gatherv(&(results[0][0]), N * tCountSize, MPI_INT,
    //             &((*global_results)[0][0]), recvcounts, displs, MPI_INT,
