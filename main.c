1fa651c67f4f6b2ea2a65fbe1c2ebb54fe068180
linuxu <linuxu@ParallelC23-2.academic.management.afeka.local>
2023-06-27 13:28:02 +0300

    ad

--- main.c
+++ main.c
@@ -4,65 +4,97 @@
 #include <stdlib.h>
 #include "myProto.h"
 
-int main(int argc, char *argv[])
-{
+void readInputFile(int* N, int* K, double* D, int* tCount, Point** points) {
+   FILE* file = fopen("input.txt", "r");
+   if (!file) {
+      fprintf(stderr, "Failed to open input file.\n");
+      MPI_Finalize();
+      exit(1);
+   }
+
+   if (fscanf(file, "%d %d %lf %d\n", N, K, D, tCount) != 4) {
+      fprintf(stderr, "Failed to read required values from input file.\n");
+      fclose(file);
+      MPI_Finalize();
+      exit(1);
+   }
+
+   *points = (Point*)malloc(*N * sizeof(Point));
+   if (!(*points)) {
+      fprintf(stderr, "Failed to allocate points.\n");
+      fclose(file);
+      MPI_Finalize();
+      exit(1);
+   }
+
+   for (int i = 0; i < *N; ++i) {
+      if (fscanf(file, "%d %lf %lf %lf %lf\n", &((*points)[i].id), &((*points)[i].x1), &((*points)[i].x2), &((*points)[i].a), &((*points)[i].b)) != 5) {
+         fprintf(stderr, "Failed to read point data from input file.\n");
+         fclose(file);
+         MPI_Finalize();
+         exit(1);
+      }
+   }
+
+   fclose(file);
+}
+
+void calculateTValues(int tCount, double** tValues) {
+   *tValues = (double*)malloc(tCount * sizeof(double));
+
+   // calculate all t points
+#pragma omp parallel for
+   for (int i = 0; i < tCount; ++i) {
+      (*tValues)[i] = 2.0 * i / tCount - 1.0;
+   }
+}
+
+// void gatherResults(int rank, int size, int N, int tCount, int tCountSize, int** results, int*** global_results) {
+//    int* recvcounts = (int*)malloc(size * sizeof(int));
+//    int* displs = (int*)malloc(size * sizeof(int));
+
+//    // Calculate the recvcounts and displacements for the 2D array
+//    for (int i = 0; i < size; i++) {
+//       recvcounts[i] = N * tCountSize;
+//       displs[i] = 0;
+//    }
+
+//    // Adjust the displacements for the 2D array
+//    for (int i = 1; i < size; i++) {
+//       displs[i] = displs[i - 1] + recvcounts[i - 1];
+//    }
+
+//    if (rank == 0) {
+//       *global_results = (int**)malloc(N * sizeof(int*));
+//       for (int i = 0; i < N; i++) {
+//          (*global_results)[i] = (int*)malloc(tCount * sizeof(int));
+//          for (int j = 0; j < tCount; j++) {
+//             (*global_results)[i][j] = -1;
+//          }
+//       }
+//    }
+
+//    // Gather the 2D array results from all processes into global_results on rank 0
+//    MPI_Gatherv(&(results[0][0]), N * tCountSize, MPI_INT,
+//                &((*global_results)[0][0]), recvcounts, displs, MPI_INT,
+//                0, MPI_COMM_WORLD);
+
+//    free(recvcounts);
+//    free(displs);
+// }
+
+int main(int argc, char* argv[]) {
    int rank, size;
    MPI_Init(&argc, &argv);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-   if (argc < 1)
-   {
-      if (rank == 0)
-      {
-         fprintf(stderr, "Usage: %s input.txt\n", argv[0]);
-      }
-      MPI_Finalize();
-      return 1;
-   }
-
    int N, K, tCount;
    double D;
-   Point *points = NULL;
+   Point* points = NULL;
 
-   if (rank == 0)
-   {
-      FILE *file = fopen("input.txt", "r");
-      if (!file)
-      {
-         fprintf(stderr, "Failed to open input file.\n");
-         MPI_Finalize();
-         return 1;
-      }
-
-      if (fscanf(file, "%d %d %lf %d\n", &N, &K, &D, &tCount) != 4)
-      {
-         fprintf(stderr, "Failed to read required values from input file.\n");
-         fclose(file);
-         MPI_Finalize();
-         return 1;
-      }
-      points = (Point *)malloc(N * sizeof(Point));
-      if (!points)
-      {
-         fprintf(stderr, "Failed to allocate points.\n");
-         fclose(file); // Close the file before returning
-         MPI_Finalize();
-         return 1;
-      }
-
-      for (int i = 0; i < N; ++i)
-      {
-         if (fscanf(file, "%d %lf %lf %lf %lf\n", &points[i].id, &points[i].x1, &points[i].x2, &points[i].a, &points[i].b) != 5)
-         {
-            fprintf(stderr, "Failed to read point data from input file.\n");
-            fclose(file); // Close the file before returning
-            MPI_Finalize();
-            return 1;
-         }
-      }
-
-      fclose(file);
+   if (rank == 0) {
+      readInputFile(&N, &K, &D, &tCount, &points);
    }
 
    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -75,104 +107,56 @@ int main(int argc, char *argv[])
    MPI_Type_contiguous(sizeof(Point), MPI_BYTE, &MPI_POINT);
    MPI_Type_commit(&MPI_POINT);
 
-   if (rank != 0)
-   {
-      points = (Point *)malloc(N * sizeof(Point));
-      if (!points)
-      {
+   if (rank != 0) {
+      points = (Point*)malloc(N * sizeof(Point));
+      if (!points) {
          fprintf(stderr, "Failed to allocate points.\n");
          MPI_Finalize();
-         return 1;
+         exit(1);
       }
    }
 
    MPI_Bcast(points, N, MPI_POINT, 0, MPI_COMM_WORLD);
 
-   double *tValues = (double *)malloc(tCount * sizeof(double));
-   // calculate all t points
-#pragma omp parallel for
-   for (int i = 0; i < tCount; ++i)
-   {
-      tValues[i] = 2.0 * i / tCount - 1.0;
-   }
+   double* tValues = NULL;
+   calculateTValues(tCount, &tValues);
 
-   int mytValuesSize = tCount / size;
+   int tCountSize = tCount / size;
    int remainingTValues = tCount % size;
-   int *sendcounts = (int *)malloc(size * sizeof(int));
-   int *senddispls = (int *)malloc(size * sizeof(int));
+   int* sendcounts = (int*)malloc(size * sizeof(int));
+   int* displs = (int*)malloc(size * sizeof(int));
 
    // Calculate the sendcounts and displacements
-   for (int i = 0; i < size; i++)
-   {
-      sendcounts[i] = (i < remainingTValues) ? mytValuesSize + 1 : mytValuesSize;
-      senddispls[i] = i * mytValuesSize + ((i < remainingTValues) ? i : remainingTValues);
+   for (int i = 0; i < size; i++) {
+      sendcounts[i] = (i < remainingTValues) ? tCountSize + 1 : tCountSize;
+      displs[i] = i * tCountSize + ((i < remainingTValues) ? i : remainingTValues);
    }
-   int tCountSize = sendcounts[rank];
-   double *myTValues = (double *)malloc(tCountSize * sizeof(double));
+   int myTValuesSize = sendcounts[rank];
+   double* myTValues = (double*)malloc(myTValuesSize * sizeof(double));
 
-   MPI_Scatterv(tValues, sendcounts, senddispls, MPI_DOUBLE, myTValues, tCountSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+   MPI_Scatterv(tValues, sendcounts, displs, MPI_DOUBLE, myTValues, myTValuesSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
    int count = 0;
    int globalCount = 0;
 
-   int **results = (int **)malloc(N * sizeof(int *));
-   for (int i = 0; i < N; i++)
-   {
-      results[i] = (int *)malloc(tCountSize * sizeof(int));
-      for (int j = 0; j < tCountSize; j++)
-      {
+   int** results = (int**)malloc(N * sizeof(int*));
+   for (int i = 0; i < N; i++) {
+      results[i] = (int*)malloc(myTValuesSize * sizeof(int));
+      for (int j = 0; j < myTValuesSize; j++) {
          results[i][j] = -1;
       }
    }
 
-
-   computeOnGPU(&count, &N, &K, &D, &tCountSize, myTValues, points, results);
-
-   // Gather results from all processes into global_results on rank 0
-   int* recvcounts = (int*)malloc(size * sizeof(int));
-   int* recvdispls = (int*)malloc(size * sizeof(int));
-
-   // Calculate the recvcounts and displacements for the 2D array
-   for (int i = 0; i < size; i++)
-   {
-      recvcounts[i] = N * tCountSize;
-      recvdispls[i] = 0;
-   }
-   
-
-   // Adjust the displacements for the 2D array
-   for (int i = 1; i < size; i++)
-   {
-      recvdispls[i] = recvdispls[i - 1] + recvcounts[i - 1];
-   }
+   computeOnGPU(&count, &N, &K, &D, &myTValuesSize, myTValues, points, results);
 
    int** global_results = NULL;
-   if (rank == 0)
-   {
-      global_results = (int**)malloc(N * sizeof(int*));
-      for (int i = 0; i < N; i++)
-      {
-         global_results[i] = (int*)malloc(tCount * sizeof(int));
-         for (int j = 0; j < tCount; j++)
-         {
-            global_results[i][j] = -1;
-         }
-         
-      }
-   }
+   // gatherResults(rank, size, N, tCount, myTValuesSize, results, &global_results);
 
-   // Gather the 2D array results from all processes into global_results on rank 0
-   MPI_Gatherv(&(results[0][0]), N * tCountSize, MPI_INT,
-               &(global_results[0][0]), recvcounts, recvdispls, MPI_INT,
-               0, MPI_COMM_WORLD);
-
-   if (rank == 0)
-   {
+   if (rank == 0) {
       printf("Global Count: %d\n", globalCount);
 
       // Deallocate global_results memory
-      for (int i = 0; i < N; i++)
-      {
+      for (int i = 0; i < N; i++) {
          free(global_results[i]);
       }
       free(global_results);
@@ -183,10 +167,9 @@ int main(int argc, char *argv[])
    free(tValues);
    MPI_Type_free(&MPI_POINT);
    free(sendcounts);
-   free(recvdispls);
+   free(displs);
    free(myTValues);
-   for (int i = 0; i < N; i++)
-   {
+   for (int i = 0; i < N; i++) {
       free(results[i]);
    }
    free(results);
